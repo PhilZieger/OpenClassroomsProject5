30fcc5a15b615c37a59c5d053a7e5020f79951c4
Phil Zieger <phil.zieger.pro@gmail.com>
2023-01-19 09:33:49 +0100

    Repair modal

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -145,16 +145,18 @@
           }
         });
       }
+      
       let index = 0,
         next = null;
 
       $(imagesCollection).each(function(i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
           index = i ;
+          
         }
       });
       next =
-        imagesCollection[index] ||
+        imagesCollection[index - 1] ||
         imagesCollection[imagesCollection.length - 1];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
@@ -192,7 +194,7 @@
           index = i;
         }
       });
-      next = imagesCollection[index] || imagesCollection[0];
+      next = imagesCollection[index + 1] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
